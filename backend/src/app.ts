1223c459ec703f92dc4497f751e263609992d932
Sergio Duran <538ser@gmail.com>
2025-06-12 11:05:20 -0600

    Created the DB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config/envs"
+import { MongoDatabase } from "./data/mongo-database"
 import { AppRoutes } from "./presentation/routes"
 import { Server } from "./presentation/server"
 
@@ -7,6 +8,12 @@ import { Server } from "./presentation/server"
 
 
 async function main () {
+
+    MongoDatabase.connect({
+        dbName: envs.MONGO_DB_NAME,
+        mongoUrl: envs.MONGO_URL,
+    })
+
     const server = new Server({
         port: envs.PORT,
         routes: AppRoutes.routes
--- backend/src/config/envs.ts
+++ backend/src/config/envs.ts
@@ -4,4 +4,7 @@ import env from 'env-var';
 
 export const envs = {
      PORT: env.get('PORT').required().asPortNumber(),
+
+     MONGO_URL: env.get('MONGO_URL').required().asString(),
+     MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
 }
