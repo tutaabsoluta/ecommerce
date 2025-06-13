887eba2f5ad1e867513532a398481192bf4b18cb
Sergio Duran <538ser@gmail.com>
2025-06-12 19:35:52 -0600

    Created a basic folder structure

---
+++ frontend/src/app/admin/create/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function CreateProductPage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+    </div>
+  );
+}
