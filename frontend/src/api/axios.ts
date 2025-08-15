4f9183b73f07a019048ca66aa85a80486a65056e
Sergio Duran <538ser@gmail.com>
2025-08-14 18:14:13 -0600

    Installed react query and config a interceptor

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,6 +4,16 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+api.interceptors.request.use((config) => {
 
+  const token = localStorage.getItem('AUTH_TOKEN');
+
+  if ( token ) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config;
+
+});
 
 export default api;
